f948811deb97d12f9e2578350729a531cb91e86c
MaksymBora <maxboraod@gmail.com>
2023-09-15 11:43:53 +0200

    add configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,24 @@
-import { combineReducers, createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer,
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
 });
 
-const enhancer = devToolsEnhancer();
+// import { combineReducers, createStore } from 'redux';
+// import { devToolsEnhancer } from '@redux-devtools/extension';
+// import { contactsReducer } from './contactsSlice';
+// import { filterReducer } from './filterSlice';
 
-export const store = createStore(rootReducer, enhancer);
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+//   filter: filterReducer,
+// });
+
+// const enhancer = devToolsEnhancer();
+
+// export const store = createStore(rootReducer, enhancer);
