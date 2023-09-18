4d34049057e42aea2b1043796a8967dd8ce093cc
MaksymBora <maxboraod@gmail.com>
2023-09-18 14:28:03 +0200

    update contacts list

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,21 +1,56 @@
 import { FiUserX } from 'react-icons/fi';
-import { ContactItem, RemoveButton } from './ContactsList.styled';
+import { MdDelete } from 'react-icons/md';
+import {
+  Thead,
+  Table,
+  TableRaw,
+  TableHor,
+  TableDataName,
+  TableRawContent,
+  TableDataNumber,
+  DeleteBtn,
+  Name,
+  TotalContacts,
+} from './ContactsList.styled';
+import Avatar from '@mui/material/Avatar';
+import { deepPurple } from '@mui/material/colors';
 
 export const ContactList = ({ filteredContacts, removeContact }) => {
   return (
-    <ul>
-      {filteredContacts.map(contact => {
-        return (
-          <ContactItem key={contact.id}>
-            <p>
-              {contact.name}: {contact.number}
-            </p>
-            <RemoveButton onClick={() => removeContact(contact.id)}>
-              <FiUserX size={21} />
-            </RemoveButton>
-          </ContactItem>
-        );
-      })}
-    </ul>
+    <>
+      <Table>
+        <Thead>
+          <TableRaw>
+            <TableHor>Name</TableHor>
+            <TableHor>Phone Number</TableHor>
+          </TableRaw>
+        </Thead>
+        <TotalContacts>
+          <p>CONTACTS(23)</p>
+        </TotalContacts>
+        <tbody>
+          {filteredContacts.map(contact => {
+            return (
+              <TableRawContent key={contact.id}>
+                <TableDataName>
+                  <Avatar
+                    sx={{ bgcolor: deepPurple[500], width: 40, height: 40 }}
+                  >
+                    O
+                  </Avatar>
+                  <Name>{contact.name}</Name>
+                </TableDataName>
+                <TableDataNumber>{contact.number}</TableDataNumber>
+                <div>
+                  <DeleteBtn onClick={() => removeContact(contact.id)}>
+                    <MdDelete size={25} />
+                  </DeleteBtn>
+                </div>
+              </TableRawContent>
+            );
+          })}
+        </tbody>
+      </Table>
+    </>
   );
 };
