332a42deafb147f28e4900972424126f2f2c1e84
MaksymBora <maxboraod@gmail.com>
2023-09-18 20:48:39 +0200

    udpate

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -5,7 +5,14 @@ import toast from 'react-hot-toast';
 
 const slice = createSlice({
   name: 'contacts',
-  initialState: { items: [] },
+  initialState: {
+    items: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+  },
   reducers: {
     addNewContact(state, action) {
       state.items.push(action.payload);
