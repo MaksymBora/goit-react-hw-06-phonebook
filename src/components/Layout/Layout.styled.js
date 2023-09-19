513cfc43010465e33ac1a146ecd4449c8c01a1e6
MaksymBora <maxboraod@gmail.com>
2023-09-19 17:46:15 +0200

    update container

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 1440px;
+  width: 1280px;
   margin-left: auto;
   margin-right: auto;
   padding-left: 30px;
